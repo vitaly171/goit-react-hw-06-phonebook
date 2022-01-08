9ba99eab094628b52c5adc4af5ee58f973f80960
vitaly171 <72026466+vitaly171@users.noreply.github.com>
2022-01-08 17:15:53 +0300

    commit "About"

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -68,7 +68,7 @@ function ContactForm() {
           type="text"
           name="name"
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+          title="The name can only consist of letters, apostrophe, dash and spaces. For example, Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan, etc."
           placeholder="Enter name"
           value={name}
           onChange={handleInputForm}
@@ -81,7 +81,7 @@ function ContactForm() {
           type="tell"
           name="number"
           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-          title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+          title="Phone number must contain digits and also can contain : spaces, dashes, parentheses and start with '+' "
           placeholder="Enter phone number"
           value={number}
           onChange={handleInputForm}
